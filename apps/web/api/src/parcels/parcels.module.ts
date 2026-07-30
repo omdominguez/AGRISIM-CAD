b2e7415a05b54ac25a2329cac7246931c81537f8
Omar Dominguez <omardominguez@Omars-MacBook-Air-2.local>
2026-07-30 09:24:53 -0400

    Correcciones

--- apps/web/api/src/parcels/parcels.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { ParcelsService } from './parcels.service';
-import { ParcelsController } from './parcels.controller';
-
-@Module({
-  providers: [ParcelsService],
-  controllers: [ParcelsController],
-})
-export class ParcelsModule {}
--- apps/web/api/src/producers/producers.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { ProducersService } from './producers.service';
-import { ProducersController } from './producers.controller';
-
-@Module({
-  providers: [ProducersService],
-  controllers: [ProducersController],
-})
-export class ProducersModule {}
